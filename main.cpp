e1aadc8d54d1e4c997f64a6439c4f0d08e1b3ebe
Антон Шмидт <schmidt.ajax@gmail.com>
2022-03-30 02:09:40 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,14 +1,58 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 class Solution {
+  struct Point {
+    int x;
+    int y;
+
+    double distanceTo(const Point& p) const {
+      const int dx = x - p.x;
+      const int dy = y - p.y;
+      return sqrt(dx * dx + dy * dy);
+    }
+  };
+
+  struct Line {
+    Line(Point p1, Point p2) {
+      const int dx = p2.x - p1.x;
+      const int dy = p2.y - p1.y;
+      a = dy;
+      b = -dx;
+      c = dx * p1.y - dy * p1.x;
+    }
+
+    double distanceTo(const Point& p) const {
+      return abs(a * p.x + b * p.y + c) / sqrt(a * a + b * b);
+    }
+
+    int a;
+    int b;
+    int c;
+  };
+
  public:
   double largestTriangleArea(vector<vector<int>>& points) {
-    // FIXME
-    return {};
+    double max_area = 0;
+    for (int i = 0; i < points.size(); ++i) {
+      for (int j = i + 1;  j < points.size(); ++j) {
+        const Point p1{points[i][0], points[i][1]};
+        const Point p2{points[j][0], points[j][1]};
+        const Line line{p1, p2};
+        double max_h = 0;
+        for (int k = j + 1; k < points.size(); ++k) {
+          const Point p3{points[k][0], points[k][1]};
+          max_h = max(max_h, line.distanceTo(p3));
+        }
+        max_area = max(max_area, 0.5 * p1.distanceTo(p2) * max_h);
+      }
+    }
+
+    return max_area;
   }
 };
 
